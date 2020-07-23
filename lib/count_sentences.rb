18afe4d0d34aee54185be19e2d29e6a0d413ffd3
Lesly Juarez <leslyj2@gmail.com>
2020-07-23 20:23:45 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -19,3 +19,34 @@ def count_sentences
     sentence_array.delete_if{|sentence| sentence.empty?}
     sentence_array.length
 end 	
+
+
+
+class String
+
+
+def sentence?
+
+    self.end_with?(".") ? true : false
+  end	  end
+
+
+  def question?	  def question?
+
+    self.end_with?("?") ? true : false
+  end	  end
+
+
+  def exclamation?	  def exclamation?
+
+    self.end_with?("!") ? true : false
+  end	  end
+
+
+  def count_sentences	  def count_sentences
+
+    sentence_array = self.split(/[.?!]/)
+    sentence_array.delete_if{|sentence| sentence.empty?}
+    sentence_array.length
+  end	  end
+end 	end
